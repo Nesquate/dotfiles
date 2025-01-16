0be4698d035f4327d9c150794949e5eaa4497fce
Nesquate <nesquate@gmail.com>
2025-01-17 00:02:35 +0800

    Config WezTerm

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,4 +1,30 @@
 local wezterm = require 'wezterm';
-return {
-    font = wezterm.font("MesloLGS NF"),
+local config = wezterm.config_builder();
+
+config.initial_rows = 30;
+config.initial_cols = 120;
+
+config.font = wezterm.font("MesloLGS NF");
+config.color_scheme = 'Whimsy';
+
+config.keys = {
+    { key = 'l', mods = 'ALT', action = wezterm.action.ShowLauncher },
+    { key = 'w', mods = 'CTRL|SHIFT', action = wezterm.action.CloseCurrentTab { confirm = true }},
 }
+
+config.ssh_domains = {
+    {
+        name = 'monka',
+        remote_address = 'monka.nesquate.local',
+        username = 'nesquate'
+    }
+}
+
+config.launch_menu = {
+    {
+        label = 'Htop',
+        args = { 'htop' }
+    }
+}
+
+return config;
